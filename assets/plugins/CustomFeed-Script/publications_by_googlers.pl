a23a2c196162ba36d97e96158ddefdc408d4e634
satojkovic <satojkovic@gmail.com>
2011-05-04 15:17:16 +0900

    create feed and entry

--- assets/plugins/CustomFeed-Script/publications_by_googlers.pl
+++ assets/plugins/CustomFeed-Script/publications_by_googlers.pl
@@ -6,33 +6,39 @@ use warnings;
 use Web::Scraper;
 use URI;
 use Encode;
+use YAML;
 
 ## get publications list
 my $url = 'http://research.google.com/pubs/papers.html';
 
 my $scraper = scraper {
     process "/html/body/div/div[2]/ul/li", 'papers[]' => scraper {
-        process "p", "info" => "TEXT";
+        process "p", "info" => 'TEXT';
         process "//p/a[1]", "link" => '@href';
         process "//p/a[1]", "author" => 'TEXT';
     };
 };
 my $res = $scraper->scrape( URI->new($url) );
 
+## feed and entries
+my $feed = {
+    title => "Publications by Googlers",
+    link => $url,
+    entries => [],
+};
+
 for my $paper (@{$res->{papers}}) {
+    # cut off brace
     $paper->{info} =~ /([^\[\]]+)/;
-    print encode("utf8", $1) . "\n";
-    print "$paper->{author} : $paper->{link}\n";
-    print "\n";
+
+    my $entry = {
+        title => $paper->{author},
+        body => $1, 
+        link => $paper->{link},
+    };
+
+    push @{$feed->{entries}}, $entry;
 }
 
-## feed and entries
-# my $feed = {
-#     title => "Publications by Googlers",
-#     link => $url,
-#     entries => [],
-# };
-
-# for my $paper (@{$res->{papers}}) {
-
-# }
+binmode STDOUT, ":utf8";
+print YAML::Dump $feed;
